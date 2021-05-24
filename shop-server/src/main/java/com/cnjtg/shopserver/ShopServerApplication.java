13833acd357436988ed73ff2e0e79c8a587345c7
cnjtg <cnjtg_mx@163.com>
2021-05-24 13:12:49 +0800

    创建项目

---
+++ shop-server/src/main/java/com/cnjtg/shopserver/ShopServerApplication.java
@@ -0,0 +1,16 @@
+package com.cnjtg.shopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cnjtg
+ */
+@SpringBootApplication
+public class ShopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopServerApplication.class, args);
+    }
+
+}
